a329e14e3ef5ea0b5ceaa66ad6c32938b6544fe9
RyoheiMiki <rjrbceb101@gmail.com>
2022-11-16 19:12:17 +0900

    wip: レイアウトの作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, Box, Center } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-
+import Head from "next/head";
 const colors = {
   brand: {
     900: "#1a365d",
@@ -10,12 +10,43 @@ const colors = {
   },
 };
 
-const theme = extendTheme({ colors });
+const fonts = {
+  heading: "'Roboto Mono',cursive;",
+  body: "'Roboto Mono','Kiwi Maru','monospace',serif;",
+  oxanium: "Oxanium', cursive",
+};
+
+const styles = {
+  global: {
+    body: {
+      backgroundColor: "#1E1E1E",
+      color: "#ffffff",
+    },
+  },
+};
+
+const theme = extendTheme({ styles, colors, fonts });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
-    </ChakraProvider>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300;400&family=Oxanium:wght@600;700&family=Roboto+Mono:ital,wght@0,400;1,300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Box
+          maxW="786px"
+          minH="100vh"
+          border={"1px solid white"}
+          mx="auto"
+          p={10}
+        >
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </>
   );
 }
