3869591d54d3daf855c48216c9928233360ec547
RyoheiMiki <rjrbceb101@gmail.com>
2022-11-17 08:49:36 +0900

    wip: Worksのモック作成まで

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,7 @@
-import "../styles/globals.css";
-import { ChakraProvider, extendTheme, Box, Center } from "@chakra-ui/react";
+import { ChakraProvider, Box, Center, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const fonts = {
-  heading: "'Roboto Mono',cursive;",
-  body: "'Roboto Mono','Kiwi Maru','monospace',serif;",
-  oxanium: "Oxanium', cursive",
-};
-
-const styles = {
-  global: {
-    body: {
-      backgroundColor: "#1E1E1E",
-      color: "#ffffff",
-    },
-  },
-};
-
-const theme = extendTheme({ styles, colors, fonts });
+import theme from "../theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,43 @@
-import { Box, Container, Avatar, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Avatar,
+  Heading,
+  Text,
+  Flex,
+  keyframes,
+  Stack,
+  VStack,
+  Divider,
+  Card,
+  CardBody,
+  Image,
+} from "@chakra-ui/react";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+// import Image from "next/image";
+import theme from "../theme";
+const flash = keyframes`
+  0% { opacity: 1; }
+  50% { opacity: 0; }
+  100% { opacity: 1;}
+`;
+
+const animation = `${flash} 1.5s linear infinite`;
 
 export default function Home() {
   return (
     <>
-      <Box w="100%" fontSize="4xl" textAlign="center" mt={10}>
-        <Heading as="h1">$ Rohei Miki`s Portfolio _</Heading>
+      <Box textAlign="center" mt={10}>
+        <Heading
+          as="h1"
+          textStyle="h1"
+          fontFamily={"'Oxanium',cursive;"}
+          fontSize="3xl"
+          _before={{ content: `"$"`, color: "#C778DD", paddingRight: "3" }}
+          _after={{ content: `"_"`, paddingLeft: "2", animation }}
+        >
+          Rohei Miki`s Portfolio
+        </Heading>
       </Box>
       <Box textAlign="center" mt={12}>
         <Avatar
@@ -16,17 +46,144 @@ export default function Home() {
           src="https://bit.ly/dan-abramov"
         />
       </Box>
-      <Box mt={12}>
-        <Heading as="h2" size="xl" noOfLines={1}>
-          # About
-        </Heading>
-        <Text>
-          こんにちは三木っていいます。よろしくお願いします。
-          いい天気ですね。まだまだやりたいことがたくさんあります。マジでよろしくな！
-          こんにちは三木っていいます。よろしくお願いします。
-          いい天気ですね。まだまだやりたいことがたくさんあります。マジでよろしくな！
-        </Text>
-      </Box>
+      <VStack as="section" align="flex-start">
+        <Box mt={12}>
+          <Heading
+            as="h2"
+            textStyle="h2"
+            _before={{ content: `"/"`, color: "#E43777", paddingRight: "3" }}
+          >
+            About
+          </Heading>
+          <Text mt="4" fontSize="xl">
+            こんにちは三木っていいます。よろしくお願いします。 いい天気ですね。
+            こんにちは三木っていいます。よろしくお願いします。 いい天気ですね。
+            こんにちは三木っていいます。よろしくお願いします。 いい天気ですね。
+          </Text>
+        </Box>
+        <Box pl={12} width="100%">
+          <Box mt={12}>
+            <Heading
+              as="h2"
+              textStyle="h2"
+              _before={{ content: `"#"`, color: "#7CDBCB", paddingRight: "3" }}
+              _after={{
+                position: "absolute",
+                top: "calc(50% - 1px)",
+                right: 0,
+                content: `""`,
+                height: "1px",
+                width: "38vw",
+                backgroundColor: "#7CDBCB",
+              }}
+            >
+              Job
+            </Heading>
+            <Text mt="4" fontSize="xl">
+              フロントエンドエンジニア
+            </Text>
+          </Box>
+          <Box mt={12}>
+            <Heading
+              as="h2"
+              textStyle="h2"
+              _before={{ content: `"#"`, color: "#7CDBCB", paddingRight: "3" }}
+              _after={{
+                position: "absolute",
+                top: "calc(50% - 1px)",
+                right: 0,
+                content: `""`,
+                height: "1px",
+                width: "38vw",
+                backgroundColor: "#7CDBCB ",
+              }}
+            >
+              Favolite
+            </Heading>
+            <Text mt="4" fontSize="xl">
+              ピアノ,neko
+            </Text>
+          </Box>
+          <Box mt={12}>
+            <Heading
+              as="h2"
+              textStyle="h2"
+              _before={{ content: `"#"`, color: "#7CDBCB", paddingRight: "3" }}
+              _after={{
+                position: "absolute",
+                top: "calc(50% - 1px)",
+                right: 0,
+                content: `""`,
+                height: "1px",
+                width: "38vw",
+                backgroundColor: "#7CDBCB",
+              }}
+            >
+              Skill
+            </Heading>
+            <Text mt="4" fontSize="xl">
+              React, Next.js, ruby, Ruby on Rails
+            </Text>
+          </Box>
+        </Box>
+      </VStack>
+      <Divider textAlign="center" m={"6rem auto 3rem auto"} width={40} />
+      <VStack as="section">
+        <Box mt={12}>
+          <Heading
+            as="h2"
+            textStyle="h2"
+            _before={{ content: `"/"`, color: "#574EC1", paddingRight: "3" }}
+          >
+            Work
+          </Heading>
+          <Text mt="4" fontSize="xl">
+            自分が作成したいろいろなものを載せていく予定です。
+            自分が作成したいろいろなものを載せていく予定です。
+            自分が作成したいろいろなものを載せていく予定です。
+          </Text>
+        </Box>
+        <VStack pt="8">
+          <Card maxW="xl">
+            <CardBody>
+              <Image
+                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                alt="Green double couch with wooden legs"
+                borderRadius="sm"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md" color={"#fff"}>
+                  Living room Sofa
+                </Heading>
+                <Text color={"#fff"}>
+                  This sofa is perfect for modern tropical spaces, baroque
+                  inspired spaces, earthy toned spaces and for people who love a
+                  chic design with a sprinkle of vintage design.
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+          <Card maxW="xl">
+            <CardBody>
+              <Image
+                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                alt="Green double couch with wooden legs"
+                borderRadius="sm"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md" color={"#fff"}>
+                  Living room Sofa
+                </Heading>
+                <Text color={"#fff"}>
+                  This sofa is perfect for modern tropical spaces, baroque
+                  inspired spaces, earthy toned spaces and for people who love a
+                  chic design with a sprinkle of vintage design.
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+        </VStack>
+      </VStack>
     </>
   );
 }
