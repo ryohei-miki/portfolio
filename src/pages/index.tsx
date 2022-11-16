8229aa80a10e2eb2ef97c6fa381acd4c45555029
RyoheiMiki <rjrbceb101@gmail.com>
2022-11-16 19:12:17 +0900

    wip: レイアウトの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Container, Avatar, Heading, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -5,7 +6,27 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <div>Hello World</div>
+      <Box w="100%" fontSize="4xl" textAlign="center" mt={10}>
+        <Heading as="h1">$ Rohei Miki`s Portfolio _</Heading>
+      </Box>
+      <Box textAlign="center" mt={12}>
+        <Avatar
+          size="2xl"
+          name="Dan Abrahmov"
+          src="https://bit.ly/dan-abramov"
+        />
+      </Box>
+      <Box mt={12}>
+        <Heading as="h2" size="xl" noOfLines={1}>
+          # About
+        </Heading>
+        <Text>
+          こんにちは三木っていいます。よろしくお願いします。
+          いい天気ですね。まだまだやりたいことがたくさんあります。マジでよろしくな！
+          こんにちは三木っていいます。よろしくお願いします。
+          いい天気ですね。まだまだやりたいことがたくさんあります。マジでよろしくな！
+        </Text>
+      </Box>
     </>
   );
 }
