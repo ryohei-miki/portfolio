7fc662504b156c422dd12631e4a81d7a5c55e4b9
RyoheiMiki <rjrbceb101@gmail.com>
2022-11-16 20:25:57 +0900

    fix: themeを別フォルダに移動

---
+++ src/theme.ts
@@ -0,0 +1,41 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const fonts = {
+  heading: "'Roboto Mono',cursive;",
+  body: "'Roboto Mono','Kiwi Maru','monospace',serif;",
+};
+
+const textStyles = {
+  h1: {
+    letterSpacing: "0.15rem",
+  },
+  h2: {
+    letterSpacing: "0.2rem",
+    size: "xl",
+    noOfLines: 1,
+  },
+  headingPrefix: {
+    color: "#C778DD",
+    paddingRight: "2",
+  },
+};
+
+const styles = {
+  global: {
+    body: {
+      backgroundColor: "#1E1E1E",
+      color: "#ffffff",
+    },
+  },
+};
+
+const theme = extendTheme({ textStyles, styles, colors, fonts });
+export default theme;
