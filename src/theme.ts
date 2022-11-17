8d6a39b10e186f220d9a5e237fcbdb464d8840d4
RyoheiMiki <rjrbceb101@gmail.com>
2022-11-18 08:20:13 +0900

    refactor: themeファイル

--- src/theme.ts
+++ src/theme.ts
@@ -1,47 +1,44 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const components = {
-  Divider: {
-    size: "3px",
-  },
-};
-
-const fonts = {
-  heading: "'Roboto Mono',cursive;",
-  body: "'Roboto Mono','Sawarabi Gothic','monospace',sans-serif;",
-};
-
-const textStyles = {
-  h1: {
-    letterSpacing: "0.15rem",
-  },
-  h2: {
-    position: "relative",
-    letterSpacing: "0.2rem",
-    noOfLines: 1,
-  },
-  headingPrefix: {
-    color: "#C778DD",
-    paddingRight: "2",
-  },
-};
-
-const styles = {
-  global: {
-    body: {
-      backgroundColor: "#191919",
-      color: "#ffffff",
+const theme = extendTheme({
+  colors: {
+    brand: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
     },
   },
-};
+  components: {
+    Divider: {
+      size: "3px",
+    },
+  },
+  fonts: {
+    heading: "'Roboto Mono',cursive;",
+    body: "'Roboto Mono','Sawarabi Gothic','monospace',sans-serif;",
+  },
+  textStyles: {
+    h1: {
+      letterSpacing: "0.15rem",
+    },
+    h2: {
+      position: "relative",
+      letterSpacing: "0.2rem",
+      noOfLines: 1,
+    },
+    headingPrefix: {
+      color: "#C778DD",
+      paddingRight: "2",
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "#191919",
+        color: "#ffffff",
+      },
+    },
+  },
+});
 
-const theme = extendTheme({ components, textStyles, styles, colors, fonts });
 export default theme;
