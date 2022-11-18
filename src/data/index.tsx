4d409676fb846d9d23be4f7b3d4ca9f9e1428913
RyoheiMiki <rjrbceb101@gmail.com>
2022-11-18 09:12:09 +0900

    refactor:  ロジックの修正

---
+++ src/data/index.tsx
@@ -0,0 +1,44 @@
+import GitHubIcon from "../../public/images/github.svg";
+import TwitterIcon from "../../public/images/twitter.svg";
+import ZennIcon from "../../public/images/zenn.svg";
+
+export const snsData = [
+  {
+    url: "https://github.com/RyoheiMiki",
+    icon: <GitHubIcon width={32} height={32} />,
+  },
+  {
+    url: "https://twitter.com/rm_oshushi",
+    icon: <TwitterIcon width={32} height={32} />,
+  },
+  {
+    url: "https://zenn.dev/rm",
+    icon: <ZennIcon width={32} height={32} />,
+  },
+];
+
+export const introData = [
+  {
+    title: "Job",
+    text: "フロントエンドエンジニア",
+  },
+  {
+    title: "Favolite",
+    text: "ピアノ,neko",
+  },
+  {
+    title: "Skill",
+    text: "React, Next.js, ruby, Ruby on Rails",
+  },
+];
+
+export const worksData = [
+  {
+    title: "Living room Sofa",
+    text: "This sofa is perfect for modern tropical spaces, baroque inspired spaces, earthy toned spaces and for people who love a chic design with a sprinkle of vintage design.",
+  },
+  {
+    title: "Living room Sofa",
+    text: "This sofa is perfect for modern tropical spaces, baroque inspired spaces, earthy toned spaces and for people who love a chic design with a sprinkle of vintage design.",
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,9 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import GitHubIcon from "../../public/images/github.svg";
-import TwitterIcon from "../../public/images/twitter.svg";
-import ZennIcon from "../../public/images/zenn.svg";
 
 import theme from "../theme";
+import { snsData, introData, worksData } from "data";
 
 const flash = keyframes`
   0% { opacity: 1; }
@@ -50,23 +48,11 @@ export default function Home() {
           src="https://bit.ly/dan-abramov"
         />
         <HStack spacing={8} justifyContent="center" mt="8">
-          <Link
-            href="https://github.com/RyoheiMiki"
-            isExternal
-            _hover={{ opacity: 0.6 }}
-          >
-            <GitHubIcon width={32} height={32} />
-          </Link>
-          <Link
-            href="https://twitter.com/rm_oshushi"
-            isExternal
-            _hover={{ opacity: 0.6 }}
-          >
-            <TwitterIcon width={32} height={32} />
-          </Link>
-          <Link href="https://zenn.dev/rm" isExternal _hover={{ opacity: 0.6 }}>
-            <ZennIcon width={32} height={32} />
-          </Link>
+          {snsData.map((item) => (
+            <Link href={item.url} key={item.url} _hover={{ opacity: 0.6 }}>
+              {item.icon}
+            </Link>
+          ))}
         </HStack>
       </Box>
       <VStack as="section" align="flex-start">
@@ -86,72 +72,36 @@ export default function Home() {
           </Text>
         </Box>
         <Box pl={12} width="100%">
-          <Box mt={12}>
-            <Heading
-              as="h2"
-              textStyle="h2"
-              fontSize="2xl"
-              _before={{ content: `"#"`, color: "#7CDBCB", paddingRight: "3" }}
-              _after={{
-                position: "absolute",
-                top: "calc(50% - 1px)",
-                right: 0,
-                content: `""`,
-                height: "1px",
-                width: { base: "0px", sm: "40%" },
-                backgroundColor: "#7CDBCB",
-              }}
-            >
-              Job
-            </Heading>
-            <Text mt="4" fontSize="lg">
-              フロントエンドエンジニア
-            </Text>
-          </Box>
-          <Box mt={12}>
-            <Heading
-              as="h2"
-              textStyle="h2"
-              fontSize="2xl"
-              _before={{ content: `"#"`, color: "#7CDBCB", paddingRight: "3" }}
-              _after={{
-                position: "absolute",
-                top: "calc(50% - 1px)",
-                right: 0,
-                content: `""`,
-                height: "1px",
-                width: { base: "0px", sm: "40%" },
-                backgroundColor: "#7CDBCB ",
-              }}
-            >
-              Favolite
-            </Heading>
-            <Text mt="4" fontSize="lg">
-              ピアノ,neko
-            </Text>
-          </Box>
-          <Box mt={12}>
-            <Heading
-              as="h2"
-              textStyle="h2"
-              fontSize="2xl"
-              _before={{ content: `"#"`, color: "#7CDBCB", paddingRight: "3" }}
-              _after={{
-                position: "absolute",
-                top: "calc(50% - 1px)",
-                right: 0,
-                content: `""`,
-                height: "1px",
-                width: { base: "0px", sm: "40%" },
-                backgroundColor: "#7CDBCB",
-              }}
-            >
-              Skill
-            </Heading>
-            <Text mt="4" fontSize="xl">
-              React, Next.js, ruby, Ruby on Rails
-            </Text>
-          </Box>
+          <VStack mt={12} alignItems="flex-start" spacing={16}>
+            {introData.map((item) => (
+              <Box key={item.title} w="100%">
+                <Heading
+                  as="h2"
+                  textStyle="h2"
+                  fontSize="2xl"
+                  _before={{
+                    content: `"#"`,
+                    color: "#7CDBCB",
+                    paddingRight: "3",
+                  }}
+                  _after={{
+                    position: "absolute",
+                    top: "calc(50% - 1px)",
+                    right: 0,
+                    content: `""`,
+                    height: "1px",
+                    width: { base: "0px", sm: "40%" },
+                    backgroundColor: "#7CDBCB",
+                  }}
+                >
+                  {item.title}
+                </Heading>
+                <Text mt={4} fontSize="lg">
+                  {item.text}
+                </Text>
+              </Box>
+            ))}
+          </VStack>
         </Box>
       </VStack>
       <Divider textAlign="center" m={"6rem auto 3rem auto"} width={40} />
@@ -172,44 +122,23 @@ export default function Home() {
           </Text>
         </Box>
         <VStack pt="8" spacing={20}>
-          <Card>
-            <CardBody p={0}>
-              <Image
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                borderRadius="sm"
-              />
-              <Stack mt="6">
-                <Heading size="md" color={"#fff"}>
-                  Living room Sofa
-                </Heading>
-                <Text color={"#fff"}>
-                  This sofa is perfect for modern tropical spaces, baroque
-                  inspired spaces, earthy toned spaces and for people who love a
-                  chic design with a sprinkle of vintage design.
-                </Text>
-              </Stack>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardBody p={0}>
-              <Image
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                borderRadius="sm"
-              />
-              <Stack mt="6">
-                <Heading size="md" color={"#fff"}>
-                  Living room Sofa
-                </Heading>
-                <Text color={"#fff"}>
-                  This sofa is perfect for modern tropical spaces, baroque
-                  inspired spaces, earthy toned spaces and for people who love a
-                  chic design with a sprinkle of vintage design.
-                </Text>
-              </Stack>
-            </CardBody>
-          </Card>
+          {worksData.map((item) => (
+            <Card key={item.title}>
+              <CardBody p={0}>
+                <Image
+                  src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  alt="Green double couch with wooden legs"
+                  borderRadius="sm"
+                />
+                <Stack mt="6">
+                  <Heading size="md" color={"#fff"}>
+                    {item.title}
+                  </Heading>
+                  <Text color={"#fff"}>{item.text}</Text>
+                </Stack>
+              </CardBody>
+            </Card>
+          ))}
         </VStack>
       </VStack>
       <Box as="footer" mt={20} textAlign="center" fontSize="sm">
