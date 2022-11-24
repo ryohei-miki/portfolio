e08c6cb924f0c977847fa2d85e4a60e7974d75b9
RyoheiMiki <rjrbceb101@gmail.com>
2022-11-24 09:18:24 +0900

    fix: アバターの修正

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,18 @@
-import { HStack, Avatar } from "@chakra-ui/react";
+import { HStack, Avatar, Box } from "@chakra-ui/react";
 import React from "react";
 import { SnsLinks } from "components/SnsLinks";
 
 export const Profile: React.FC = () => (
   <>
-    <Avatar
-      w={40}
-      h={40}
-      name="Dan Abrahmov"
-      src="https://bit.ly/dan-abramov"
-    />
+    <Box>
+      <Avatar
+        w={40}
+        h={40}
+        name="avatar"
+        src="../../images/avatar.png"
+        // showBorder
+      />
+    </Box>
     <HStack spacing={8} justifyContent="center" mt="8">
       <SnsLinks />
     </HStack>
